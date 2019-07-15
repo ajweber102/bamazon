1ca51aa3e57d3b7254ba70f4b2173e667b309d16
AJW <ajweber102@gmail.com>
2019-07-15 14:04:20 -0400

    Created products table in database

--- products.sql
+++ products.sql
@@ -1,1 +1,17 @@
+-- Dropping DB as it exists--
+DROP DATABASE IF EXISTS bamazon_db;
+
+-- Creating the DB again --
 CREATE DATABASE bamazon_db;
+
+-- Targeting bamazon database --
+USE bamazon_db;
+
+-- Creating the products table --
+CREATE TABLE products (
+	item_id INTEGER(30) NOT NULL,
+    product_name VARCHAR (30) NOT NULL,
+    department_name VARCHAR (30) NOT NULL,
+    price INTEGER(30) NOT NULL,
+    stock_quantity INTEGER(100) NOT NULL
+);
