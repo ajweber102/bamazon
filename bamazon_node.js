c59e6bd39da136312fa20380b563635eac2f940c
AJW <ajweber102@gmail.com>
2019-07-16 10:18:11 -0400

    Set up connection with localhost and added error state

--- bamazon_node.js
+++ bamazon_node.js
@@ -2,3 +2,16 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
 var cliTable = require("cli-table");
+
+var connection = mysql.addConnection({
+    host:"localhost",
+    port:8080,
+    user:"root",
+    password:"",
+    database:"bamazon"
+});
+
+connection.connect(function(err){
+    if(err)throw err;
+    console.log("Connected ID is " + connection.threadId);
+});
