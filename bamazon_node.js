7abbf1a0fa2884513172fee82f2a7253fd781d2b
AJW <ajweber102@gmail.com>
2019-07-16 11:43:45 -0400

    Created function for showing product table for user

--- bamazon_node.js
+++ bamazon_node.js
@@ -5,7 +5,8 @@ var cliTable = require("cli-table");
 
 var connection = mysql.addConnection({
     host:"localhost",
-    port:8080,
+    // 3306 is the port for the MySQL Open Source Database
+    port:3306,
     user:"root",
     password:"",
     database:"bamazon"
