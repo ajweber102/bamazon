180f954d95efb9a82082b7129f34c51e8d4443d6
AJW <ajweber102@gmail.com>
2019-07-17 15:36:03 -0400

    Final touches and formatting

--- bamazon_node.js
+++ bamazon_node.js
@@ -3,23 +3,25 @@ var inquirer = require("inquirer");
 var mysql = require("mysql");
 var cliTable = require("cli-table");
 
-var connection = mysql.createConnection({
+var connection = mysql.createConnection( {
     host:"localhost",
     // 3306 is the port for the MySQL Open Source Database
     port: 3306,
+    // Username and Pass
     user:"root",
     password:"P3pp3r102!",
+    // Database name
     database:"bamazon_db"
 });
 
-connection.connect(function(err){
+connection.connect(function(err) {
     if(err)throw err;
     console.log("Connected ID is " + connection.threadId);
 });
 
 var products = function(){
     var query = "SELECT * FROM products";
-    connection.query(query, function(err, res){
+    connection.query(query, function(err, res) {
         if(err) throw err;
         // Establishing the table as referenced in SQL
         var showTable = new cliTable ({
@@ -27,7 +29,7 @@ var products = function(){
             colWidths: [15,20,20,15,15]
         });
         // Looping through products to push to location
-        for(var i = 0; i < res.length; i++){
+        for(var i = 0; i < res.length; i++) {
             showTable.push(
                 [res[i].item_id,res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]);
         }
@@ -37,17 +39,19 @@ var products = function(){
 }
 
 // Prompting the user -- interacting via node.
-function userPrompt(){
+function userPrompt() {
     inquirer.prompt([
         {
             name: "ID",
             type: "input",
-            message: "Welcome! Enter the Item ID for the item you would like to buy."
+            message: "-------Welcome! Enter the Item ID for the item you would like to buy.-------",
+            filter: Number
         },
         {
             name: "Quantity",
             type: "input",
-            message: "How many of these item(s) would you like to buy?"
+            message: "-------How many of these item(s) would you like to buy?-------",
+            filter: Number
         },
     ]).then(function(userResponse){
         var quantity = userResponse.Quantity;
@@ -56,19 +60,19 @@ function userPrompt(){
     });
 };
 
-function fulfillOrder(amount, ID){
+function fulfillOrder(amount, ID) {
     connection.query('SELECT * FROM products WHERE item_id = ' + ID, function(err,res){
         if(err){console.log(err)};
-        if(amount <= res[0].stock_quantity){
+        if(amount <= res[0].stock_quantity) {
             var total = res[0].price * amount;
-            console.log("We have you item in stock for purchase.");
-            console.log("The total cost is " + total);
+            console.log("-------We have you item in stock for purchase.-------");
+            console.log("-------The total cost is " + total + "-------");
 
             // Update the products in the source table
             connection.query("UPDATE products SET stock_quantity = stock_quantity - " + amount + "WHERE item_id = " + ID);
         
         } else {
-            console.log("Item is out of stock");
+            console.log("-------Item is out of stock-------");
         };
 
         //Order fulfillment display
